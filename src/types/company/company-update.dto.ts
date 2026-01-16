19ad9f48166b3062d37533cb5428a3628114c1d3
ShadowAdi <shadowshukla76@gmail.com>
2026-01-17 01:08:32 +0530

    added update company dto

---
+++ src/types/company/company-update.dto.ts
@@ -0,0 +1,19 @@
+import { IAddress } from "../../models/interfaces/company.interface";
+
+export interface UpdateCompanyDTO {
+  company_name?: string;
+  company_email?: string;
+  password?: string;
+
+  company_description?: string;
+  address?: IAddress;
+
+  website?: string;
+  phone?: string;
+  company_logo?: string;
+  industury?: string;
+  company_linkedin?: string;
+
+  founder_email?: string;
+  founder_name?: string;
+}
