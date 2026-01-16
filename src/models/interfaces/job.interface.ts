2d2b558fec3107a0d64c139229e8dfe249ed866b
ShadowAdi <shadowshukla76@gmail.com>
2026-01-17 02:58:12 +0530

    added job interfaces

---
+++ src/models/interfaces/job.interface.ts
@@ -0,0 +1,32 @@
+import { Document, Types } from "mongoose";
+
+export interface IJob extends Document {
+  createdBy: Types.ObjectId;
+
+  jobTitle: string;
+  jobDescription: string;
+
+  jobRole:
+    | "frontend"
+    | "backend"
+    | "fullstack"
+    | "mobile"
+    | "data"
+    | "embed"
+    | "other"
+    | string;
+
+  seniorityLevel: "intern" | "junior" | "mid" | "senior" | string;
+
+  techStack: string[];
+
+  employmentType?: "internship" | "full-time" | "contract" | "part-time";
+  location?: string;
+
+  lastDateToApply?: Date;
+
+  status: "draft" | "active" | "archived";
+
+  createdAt: Date;
+  updatedAt: Date;
+}
