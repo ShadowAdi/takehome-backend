6ef1f9e82c4ebbe3e66d225caf0e6a6f85eb4db3
ShadowAdi <shadowshukla76@gmail.com>
2026-01-16 23:24:11 +0530

    added listen event

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,15 @@ import express from "express";
 import helmet from "helmet";
 import { CorsConfig } from "./config/cors";
 import "./config/dotenv"
+import { PORT } from "./config/dotenv";
+import { logger } from "./config/logger";
 
 const app=express()
 
 app.use(helmet())
 CorsConfig(app)
+
+app.listen(PORT,()=>{
+    console.log(`Your app server is running at port: ${PORT}`)
+    logger.info(`Your app server is running at port: ${PORT}`)
+})
