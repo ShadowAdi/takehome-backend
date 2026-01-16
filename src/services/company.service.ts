6f9a08d7c0820d33ddff54f7a4f542c150c01531
ShadowAdi <shadowshukla76@gmail.com>
2026-01-17 01:19:09 +0530

    create company service

---
+++ src/services/company.service.ts
@@ -0,0 +1,48 @@
+// services/company.service.ts
+import bcrypt from "bcrypt";
+import { logger } from "../config/logger";
+import { CreateCompanyDTO } from "../types/company/company-create.dto";
+import { AppError } from "../utils/AppError";
+import { Company } from "../models/company.model";
+import { hashPassword } from "../utils/password";
+
+class CompanyService {
+  async createCompany(payload: CreateCompanyDTO) {
+    try {
+      const exists = await Company.findOne({
+        $or: [
+          { company_email: payload.company_email },
+          { company_name: payload.company_name }
+        ]
+      });
+
+      if (exists) {
+        throw new AppError("Company already exists", 409);
+      }
+
+      const hashedPassword = await hashPassword(payload.password);
+
+      const company = await Company.create({
+        ...payload,
+        password: hashedPassword
+      });
+
+      return {
+        id: company._id,
+        company_name: company.company_name,
+        company_email: company.company_email,
+        company_description: company.company_description,
+        createdAt: company.createdAt
+      };
+
+    } catch (error: any) {
+      logger.error(`Failed to create company service: ${error.message}`);
+      console.error(`Failed to create company service: ${error.message}`);
+      throw error instanceof AppError
+        ? error
+        : new AppError("Internal Server Error", 500);
+    }
+  }
+}
+
+export const companyService = new CompanyService();
