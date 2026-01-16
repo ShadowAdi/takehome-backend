9ae05e6d5c420d381281b82a4d6e153f77f35cc3
ShadowAdi <shadowshukla76@gmail.com>
2026-01-16 23:30:33 +0530

    added initialize connection

---
+++ src/config/initialize.db.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import { DB_URL } from "./dotenv";
+import { logger } from "./logger";
+
+export const initializeConnection = async () => {
+  if (!DB_URL) {
+    logger.error(`Cant Connect To Db URL Does not exist`);
+    console.log(`Cant Connect To Db URL Does not exist`);
+    return;
+  }
+  try {
+    await mongoose
+      .connect(DB_URL!)
+      .then(() => {
+        logger.info(`Connected Successfully`);
+        console.log(`Connected Successfully`);
+      })
+      .catch((err) => {
+        logger.error(`Failed to connect to DB: ${err}`);
+        console.error(`Failed to connect to DB: ${err}`);
+      });
+  } catch (err) {
+    logger.error(`Failed to connect to DB: ${err}`);
+    console.error(`Failed to connect to DB: ${err}`);
+    return;
+  }
+};
--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,16 @@ import { CorsConfig } from "./config/cors";
 import "./config/dotenv"
 import { PORT } from "./config/dotenv";
 import { logger } from "./config/logger";
+import { initializeConnection } from "./config/initialize.db";
 
-const app=express()
+const app = express()
 
 app.use(helmet())
 CorsConfig(app)
 
-app.listen(PORT,()=>{
+app.listen(PORT, async () => {
     console.log(`Your app server is running at port: ${PORT}`)
     logger.info(`Your app server is running at port: ${PORT}`)
+
+    await initializeConnection()
 })
