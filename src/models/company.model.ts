4138bb088495a15488f9ca3cdd660607a3b7883b
ShadowAdi <shadowshukla76@gmail.com>
2026-01-17 00:51:31 +0530

    added company schema

---
+++ src/models/company.model.ts
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+
+const AddressSchema = new mongoose.Schema({
+    street: { type: String, trim: true },
+    city: { type: String, trim: true },
+    state: { type: String, trim: true },
+    zip: { type: String, trim: true },
+    country: { type: String, trim: true }
+}, { _id: false });
+
+export const companySchema = new mongoose.Schema({
+    company_name: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    company_email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    company_description: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    address: AddressSchema,
+    website: {
+        type: String,
+        trim: true
+    },
+    phone: {
+        type: String,
+    },
+    company_logo: {
+        type: String,
+    },
+    industury: {
+        type: String,
+    },
+    company_linkedin: {
+        type: String,
+    },
+    founder_email: {
+        type: String,
+        trim: true,
+    },
+    founder_name: {
+        type: String,
+        trim: true
+    }
+}, {
+    timestamps: true
+})
+
+export const Company = mongoose.models.Company || mongoose.model("Company", companySchema)
