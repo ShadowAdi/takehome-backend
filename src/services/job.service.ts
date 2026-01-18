be4a707ffe1a8c6f0f5f72df3adfd2e3d0179876
ShadowAdi <shadowshukla76@gmail.com>
2026-01-18 18:27:42 +0530

    added delete job

--- src/services/job.service.ts
+++ src/services/job.service.ts
@@ -126,4 +126,29 @@ class JobService {
                 : new AppError("Internal Server Error", 500);
         }
     }
+
+    async deleteJob(jobId: string) {
+        try {
+            const getJob = await Job.exists({
+                _id: jobId
+            })
+            if (!getJob) {
+                logger.error(`Job with id: ${jobId} not found`);
+                console.error(`Job with id: ${jobId} not found`);
+                throw new AppError(`Job with id: ${jobId} not found`, 500);
+            }
+
+            await Job.findByIdAndDelete(
+                jobId
+            );
+
+            return `Job with id: ${jobId} got deleted successfully`;
+        } catch (error: any) {
+            logger.error(`Failed to delete job by id:${jobId}  error: ${error.message}`);
+            console.error(`Failed to delete job by id:${jobId} error: ${error.message}`);
+            throw error instanceof AppError
+                ? error
+                : new AppError("Internal Server Error", 500);
+        }
+    }
 }
