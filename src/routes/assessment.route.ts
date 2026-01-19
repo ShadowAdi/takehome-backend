06723665074bb3fa1dc458b73521a90dd4b22959
ShadowAdi <shadowshukla76@gmail.com>
2026-01-19 15:17:05 +0530

    added assessment router

---
+++ src/routes/assessment.route.ts
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+import { AssessmentController } from '../controllers/assessment.controller';
+import { validate } from '../middlewares/validation.middleware';
+import {
+    createAssessmentValidator,
+    updateAssessmentValidator,
+    getAssessmentByIdValidator,
+    getAssessmentByUniqueIdValidator,
+    deleteAssessmentValidator,
+    getAssessmentsByJobIdValidator,
+    getAssessmentsByCompanyIdValidator
+} from '../validator/assessment.validator';
+
+export const assessmentRouter = Router();
+
+// Create assessment for a job
+assessmentRouter.post('/job/:jobId', getAssessmentsByJobIdValidator, validate, createAssessmentValidator, validate, AssessmentController.createAssessment);
+
+// Get all assessments by job ID
+assessmentRouter.get('/job/:jobId', getAssessmentsByJobIdValidator, validate, AssessmentController.getAllAssessmentsByJob);
+
+// Get all assessments by company ID
+assessmentRouter.get('/company/:companyId', getAssessmentsByCompanyIdValidator, validate, AssessmentController.getAllAssessmentsByCompanyId);
+
+// Get assessment by unique ID
+assessmentRouter.get('/unique/:uniqueId', getAssessmentByUniqueIdValidator, validate, AssessmentController.getSingleAssessmentByUniqueId);
+
+// Get assessment by ID
+assessmentRouter.get('/:assessmentId', getAssessmentByIdValidator, validate, AssessmentController.getSingleAssessment);
+
+// Update assessment
+assessmentRouter.patch('/:assessmentId', updateAssessmentValidator, validate, AssessmentController.updateAssessment);
+
+// Delete assessment
+assessmentRouter.delete('/:assessmentId', deleteAssessmentValidator, validate, AssessmentController.deleteAssessment);
