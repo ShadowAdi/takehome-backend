da25dc4ff0a0bf1231d0698bb9cf4c803e622eec
ShadowAdi <shadowshukla76@gmail.com>
2026-01-19 12:25:06 +0530

    added job routes

--- src/routes/job.routes.ts
+++ src/routes/job.routes.ts
@@ -1,5 +1,20 @@
 import { Router } from "express";
+import { createJobValidator, deleteJobValidator, getJobByIdValidator, updateJobStatusValidator, updateJobValidator } from "../validator/job.validator";
+import { validate } from "../middlewares/validation.middleware";
+import { JobController } from "../controllers/job.controller";
 
 export const jobRouter = Router();
+
+jobRouter.post('/', createJobValidator, validate, JobController.createJob);
+
+jobRouter.get('/', JobController.getAllJobs);
+
+jobRouter.get('/:id', getJobByIdValidator, validate, JobController.getJobById);
+
+jobRouter.patch('/update/status', updateJobStatusValidator, validate, JobController.updateJobStatus);
+
+jobRouter.patch('/:id', updateJobValidator, validate, JobController.updateJob);
+
+jobRouter.delete('/:id', deleteJobValidator, validate, JobController.deleteJob);
 
 
