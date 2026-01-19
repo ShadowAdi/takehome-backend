ce71575fd892a3509700c98fa4e40a7e5e327703
ShadowAdi <shadowshukla76@gmail.com>
2026-01-19 14:30:17 +0530

    added create assessment

--- src/services/assessment.service.ts
+++ src/services/assessment.service.ts
@@ -1,5 +1,50 @@
+import { logger } from "../config/logger";
+import { Assessment } from "../models/assessment.model";
+import { CreateAssessmentDto } from "../types/assessment/assessment-create.dto";
+import { AppError } from "../utils/AppError";
+import { jobService } from "./job.service";
+
 class AssessmentService {
-    
+    async createAssessment(payload: CreateAssessmentDto, jobId: string) {
+        try {
+            const exists = await jobService.getJob(jobId);
+
+            if (!exists) {
+                logger.error(`Cant create assessment if job does not exist in the first place`)
+                console.error(`Cant create assessment if job does not exist in the first place`)
+                throw new AppError(`Cant create assessment if job does not exist in the first place`, 401)
+            }
+
+            const isAlreadyExist = await Assessment.findOne({
+                title: payload.title.toLowerCase().trim(),
+                jobId: jobId
+            })
+
+            if (!isAlreadyExist) {
+                logger.error(`Already Exist try to update previous`)
+                console.error(`Already Exist try to update previous`)
+                throw new AppError(`Already Exist try to update previous`, 401)
+            }
+
+            const uniqueJobId = crypto.randomUUID().slice(-6)
+
+
+
+            const assessment = await Assessment.create({
+                ...payload,
+                jobId: jobId,
+                uniqueId: uniqueJobId
+            });
+
+            return assessment
+        } catch (error: any) {
+            logger.error(`Failed to create assessment service: ${error.message}`);
+            console.error(`Failed to create assessment service: ${error.message}`);
+            throw error instanceof AppError
+                ? error
+                : new AppError("Internal Server Error", 500);
+        }
+    }
 }
 
 export const assessmentService = new AssessmentService();
