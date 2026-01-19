eced0351ec6affb2c1c2bcc62eb63c9a3efee84c
ShadowAdi <shadowshukla76@gmail.com>
2026-01-19 15:08:05 +0530

    creatre assessmnet controller

--- src/controllers/assessment.controller.ts
+++ src/controllers/assessment.controller.ts
@@ -1,7 +1,30 @@
+import { NextFunction, Request, Response } from "express";
+import { CreateAssessmentDto } from "../types/assessment/assessment-create.dto";
+import { assessmentService } from "../services/assessment.service";
+import { logger } from "../config/logger";
 
 
 class AssessmentControllerClass {
- 
+    async createAssessment(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { jobId } = req.params
+            if (Array.isArray(jobId)) {
+                throw new Error("Invalid identifier parameter");
+            }
+            const { payload, companyId }: { payload: CreateAssessmentDto, companyId: string } = req.body;
+            const job = await assessmentService.createAssessment(payload, jobId, companyId);
+
+            res.status(201).json({
+                success: true,
+                message: "Assessment created successfully",
+                data: job,
+            });
+        } catch (error) {
+            logger.error(`Failed to create job: ${error}`);
+            console.error(`Failed to create job: ${error}`);
+            next(error);
+        }
+    }
 }
 
 export const AssessmentController = new AssessmentControllerClass();
