7517a1a214f90b924a4cbdbadc78f7f417a7d4a5
ShadowAdi <shadowshukla76@gmail.com>
2026-01-20 03:19:25 +0530

    added ai dto

--- src/types/assessment/assessment-create.dto.ts
+++ src/types/assessment/assessment-create.dto.ts
@@ -40,3 +40,44 @@ export interface CreateAssessmentDto {
     evaluation?: string;
     status?: "draft" | "active" | "closed";
 }
+
+export interface CreateAssessmentAIDto {
+    title: string
+    problem_description: string;
+    allowedTechStack?: string;
+    instructions?: string;
+    constraints?: string;
+    expectedDurationHours: number;
+    submissionDeadlineDays: number;
+    submissionRequirements: {
+        githubUrl: {
+            required: boolean;
+            description: string
+        },
+        deployedUrl: {
+            required: boolean;
+            description: string
+        },
+        videoDemo: {
+            required: boolean;
+            description: string;
+            platform: string;
+        }
+        documentation: {
+            required: boolean;
+            description: string
+        },
+        otherUrls: [{
+            required: boolean;
+            description: string;
+            label: string
+        }],
+        additionalInfo: {
+            required: boolean,
+            placeholder: string,
+            maxLength: number,
+        }
+    };
+    limitations?: string;
+    evaluation?: string;
+}
