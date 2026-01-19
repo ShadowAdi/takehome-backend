d225c845ce585f82d62d10795549f5463961964b
ShadowAdi <shadowshukla76@gmail.com>
2026-01-19 13:47:03 +0530

    added get assessment schema

---
+++ src/types/assessment/assessment.dto.ts
@@ -0,0 +1,48 @@
+import { Types } from "mongoose";
+
+export interface GetAssessmentPublicDTO {
+    id: string;
+    title: string;
+    jobId: string | Types.ObjectId;
+    problem_description: string;
+    allowedTechStack?: string;
+    instructions?: string;
+    constraints?: string;
+    expectedDurationHours: number;
+    submissionDeadlineDays: number;
+    submissionRequirements: {
+        githubUrl: {
+            required: boolean;
+            description: string
+        },
+        deployedUrl: {
+            required: boolean;
+            description: string
+        },
+        videoDemo: {
+            required: boolean;
+            description: string;
+            platform: string;
+        }
+        documentation: {
+            required: boolean;
+            description: string
+        },
+        otherUrls: [{
+            required: boolean;
+            description: string;
+            label: string
+        }],
+        additionalInfo: {
+            required: boolean,
+            placeholder: string,
+            maxLength: number,
+        }
+    };
+    limitations?: string;
+    evaluation?: string;
+    status?: "draft" | "active" | "closed";
+    uniqueId:string;
+    createdAt: Date;
+    updatedAt: Date;
+}
