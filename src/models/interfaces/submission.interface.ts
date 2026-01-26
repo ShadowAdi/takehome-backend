8da994909199fa0b06c267ae2f9823bf3ce5d3a6
ShadowAdi <shadowshukla76@gmail.com>
2026-01-26 12:37:47 +0530

    improve submission interface

--- src/models/interfaces/submission.interface.ts
+++ src/models/interfaces/submission.interface.ts
@@ -30,9 +30,45 @@ export interface ISubmission extends Document {
         additionalInfo?: string;
     };
 
-    status: "submitted" | "under_review" | "evaluated" | "rejected";
+    status: 
+        | "submitted"
+        | "under_review"
+        | "rejected"
+        | "selected"
+        | "on_hold";
+
     score?: number;
     feedback?: string;
+
+    decision?: {
+        outcome: "reject" | "select" | "hold";
+        messageToCandidate?: string; // shown on UI
+        decidedAt?: Date;
+    };
+
+    nextSteps?: {
+        type: "meeting" | "call" | "email" | "task";
+
+        description?: string;
+
+        meeting?: {
+            platform: "Google Meet" | "Zoom" | "Teams" | string;
+            meetingLink?: string;
+            scheduledAt?: Date;
+            durationMinutes?: number;
+        };
+
+        contact?: {
+            email?: string;
+            phone?: string;
+        };
+
+        task?: {
+            title: string;
+            instructions: string;
+            deadline?: Date;
+        };
+    };
 
     submittedAt: Date;
     evaluatedAt?: Date;
